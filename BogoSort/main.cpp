0cbf373c5a283bb9b58238c5f85277f39822d30f
DariusL <d.lapunas@gmail.com>
2013-08-03 21:09:02 +0300

    multithreaded

--- BogoSort/main.cpp
+++ BogoSort/main.cpp
@@ -4,48 +4,83 @@
 #include <random>
 #include <chrono>
 #include <climits>
+#include <thread>
+#include <future>
+#include <Windows.h>
 
 using namespace std;
 
-void bogosort(vector<int> &stuff, long long unsigned &count, long long unsigned &overflowCount);
+struct SortResult
+{
+	unsigned long long count;
+	unsigned long long overflows;
+	vector<int> vec;
+};
+
+SortResult bogosort(vector<int> &vec);
 bool isSortedAsc(vector<int> &stuff);
 
 int main()
 {
 	int inputSize = 20;
+	int threadCount = 3;
 	
 	cout << "Bogosorting ints, input size from 0 to " << inputSize << endl;
 	cout << "Counting in unsigned long long, also counting overflows" << endl << endl;
+
+	vector<future<SortResult>> futures;
+
 	for(int i = 1; i <= inputSize; i++)
 	{
 		vector<int> vec;
 		vec.reserve(i);
+
 		for(int j = 0; j < i; j++)
 			vec.push_back(j);
 		random_shuffle(vec.begin(), vec.end());
-		long long unsigned count, overflowCount;
-		bogosort(vec, count, overflowCount);
-		cout << "input size: " << i << endl 
-			<< count << " counts, " << overflowCount << " overflows" << endl;
+
+		for(int j = 0; j < threadCount; j++)
+		{
+			futures.push_back(async(bogosort, vec));
+		}
+
+		int finished = 0;
+		while(1)
+		{
+			if(futures[finished].wait_for(chrono::seconds(0)) == future_status::ready)
+				break;
+			Sleep(100);
+			finished++;
+			finished = finished % threadCount;
+		}
+
+		SortResult res = futures[finished].get();
+		if(isSortedAsc(res.vec))
+			cout << "input size: " << i << endl 
+			<< res.count << " counts, " << res.overflows << " overflows" << endl;
+		futures.clear();
 	}
 	system("pause");
 	return 0;
 }
 
-void bogosort(vector<int> &stuff, long long unsigned &count, long long unsigned &overflowCount)
+SortResult bogosort(vector<int> &vec)
 {
-	count = 0;
-	overflowCount = 0;
-	while (!isSortedAsc(stuff))
+	SortResult res;
+	res.count = 0;
+	res.overflows = 0;
+	res.vec = vec;
+	while (!isSortedAsc(res.vec))
 	{
-		random_shuffle(stuff.begin(), stuff.end());
-		count++;
-		if(count == ULLONG_MAX)
+		random_shuffle(res.vec.begin(), res.vec.end());
+		res.count++;
+		if(res.count == ULLONG_MAX)
 		{
-			count = 0;
-			overflowCount++;
+			res.count = 0;
+			res.overflows++;
 		}
 	}
+	return res;
 }
 
 bool isSortedAsc(vector<int> &stuff)
